a714eb5eb41eac41cb3bf9305923bf6ad4197590
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-05 21:21:04 +0100

    Edit file

--- warehouse_manager.py
+++ warehouse_manager.py
@@ -1,13 +1,17 @@
 items = {"Name": [" dresses"," jackets", "jeans"], 
 "Quantity":["5","4","7"] ,
 "Unit":["pcs","pcs","pcs"],
-"Unit_Price (PLN)":["100","160","120"]
+"Unit_Price (PLN)":["110","160","120"]
 }
 print("What would you like to do?")
 
 def get_items():
     text == "show"
+    items_list = items.values()
     print("Name\tQuantity\tUnit\tUnit Price (PLN)")      
+    print("dresses\t5       \tpcs     \t110")
+    print("jackets\t4       \tpcs     \t160")
+    print("jeans\t7         \tpcs    \t120" )
 
 if __name__ == "__main__":
     text = input()
