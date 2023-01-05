eff01559190557b9f9568bde798f23431c93d271
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-05 20:20:27 +0100

    Edit file

--- warehouse_manager.py
+++ warehouse_manager.py
@@ -1,18 +1,18 @@
 items = {"Name": [" dresses"," jackets", "jeans"], 
-"Quantity":[""] ,
-"Unit":[""],
-"Unit_Price (PLN)":[""]
+"Quantity":["5","4","7"] ,
+"Unit":["pcs","pcs","pcs"],
+"Unit_Price (PLN)":["100","160","120"]
 }
 print("What would you like to do?")
 if __name__ == "__main__":
     text = input()
-    
+    word = "show"
     while exit:
         print("Exiting...Bye!")
         break
-    if not exit:
-        print("show")
-        
+    if word:
+         print("Name\tQuantity\tUnit\tUnit Price (PLN)")
+
 def get_items():
-    print("Name\tQuantity\tUnit\tUnit Price (PLN)")
+   return dict.values
 get_items()
