289db3224abcb6fc324a7f1701bde073ed11c911
Anna Marushchak <maruschakanya86@gmail.com>
2023-01-16 10:24:22 +0100

    Edit file

--- warehouse_manager.py
+++ warehouse_manager.py
@@ -54,14 +54,56 @@ def export_items_to_csv():
 
 
 def export_sales_to_csv():
+    with open('magazyn_sales.csv','w', newline='') as csvfile:
+        fieldnames = ['Name','Quantity','Unit', 'Unit_price PLN']
+        writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        writer.writeheader()
+        for j in range (0, len(sold_items)):
+            writer.writerow({'Name': item[j]['Name'],'Quantity':items[j]['Quantity'],'Unit':items[j]['Unit'],'Unit_price (PLN)' : ítems[j]['Unit_price (PLN)']})
+
+def load_items_from_csv():
+    items.clear()
+    with open('magazyn.csv',newline='') as csvfile:
+        reader = scv.DictReader(csvfile)
+        for row in reader:
+            print(row['Name'], row['Quantity'], row['Unit'], row ['Unit_price' (PLN)])
+            items.append({'Name': row['Name'], 'Quantity':int(row['Quantity']), 'Unit': row['Unit'], 'Unit_price (PLN)': float(row['Unit_price (PLN)']) })
 
 
 if __name__ == "__main__":
-    text = input()
-    
-    while text == "exit":
-        print("Exiting...Bye!")
-        break
-    else:
-        get_items()
+    while True:
+        action = input ("What would you like to do?")
+        if action == "exit":
+            print("Exiting...Bye!")
+        if action == "show":
+            get_items()
+        if action == "add":
+            name = input("Adding to warehouse\n Item name:")
+            quantity = int(input("Item quantity:"))
+            unit = input("Item unit:")
+            unit_price = float(input("Item unit_price:"))
+            print("Successfully added to warehouse.Current status:")
+            add_item(name, quantity,unit,unit_price)
+            get_items()
+        if action == "sell":
+            name_sell = input("Item name:")
+            quantity_sell = int(input("Quantity to sell:"))
+            sell_item(name_sell, quantity_sell)
+            get_items()
+        if action == "show revenue":
+            print(f"Revenue: {show_revenue()}")
+        if action == "save":
+            export_items_to_csv()
+            print("Successfully exported data to magazyn.csv")
+            export_sales_to_scv()
+            print("Successfully exported data to magazyn_sales.csv")
+        if action == "load":
+            load_items_from_csv()
+            print("Successfully loaded data from magazyn.csv")
+
+
+
+
+        
+       
         
